a754b1ab8ebcb4096708e76c82657003b6697d5b
tanisqcodes <the.crektek@gmail.com>
2025-08-31 23:54:41 +0530

    some routine work

---
+++ src/index.js
@@ -0,0 +1,54 @@
+// import mongoose from 'mongoose'
+ // import {DB_NAME} from "./constants"
+// require('dotenv').config({ path:  './env'})
+import connectDB from "./db/index.js"
+
+import express from "express"
+const app = express()
+
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+.then(() => { 
+    app.listen( process.env.PORT || 8000  , () => { 
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => { 
+    console.log("MONGODB CONNECTION ERROR::", err)
+
+})
+
+
+
+
+
+
+/*
+
+{ async ()=> { 
+    try{  
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("error", error )
+            throw error
+        })  // this is a listener for error
+        app.listen(process.env.PORT, ()=>{ 
+            console.log(`App is listening on port ${process.env.PORT}`)
+
+        })
+
+    }catch(error){
+        console.log("ERROR: ", error)
+        throw error
+
+    }
+}
+
+}
+*/
---
+++ src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, {Schema } from "mongoose"
+import bcrypt from "bcrypt"
+const userSchema = new Schema({
+    username: {
+        type: String , 
+        required : true , 
+        unique: true, 
+        lowercase: true,
+        trim: true , 
+        index: true,
+    }, 
+    email: { 
+        type: String , 
+        required: true , 
+        unique: true, 
+        lowercase: true, 
+        trim: true , 
+        index: true
+
+    }, 
+    fullName: { 
+        type: String , 
+        required: true , 
+        
+        trim: true , 
+        index: true
+
+    }, 
+    avatar: {
+        type: String , // cloudinary url
+        required: true, 
+
+    }, 
+    coverImage: {
+        type: String, 
+
+    }, 
+    watchHistory: [
+        {
+            type:Schema.Types.ObjectId, 
+            ref: "Video"
+        }
+    ], 
+    password: {
+type: String, 
+required: [true, 'password is required ']
+
+
+
+    }, 
+    refreshToken: {
+        type: String
+    }
+
+}, {timestamps: true})
+
+
+userSchema.pre("save", async function (next){
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password, 10)
+        next()
+
+    }else{  
+        return next()
+
+
+    }
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password , this.password) //this returns a true or false
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        { // giving payload
+            _id: this._id,
+            email: this.email, 
+            username: this .username, 
+            fullName: this.fullName, // this.fullName is from database , fullname is key in payload
+
+
+
+        }, process.env.ACCESS_TOKEN_SECRET, 
+        {
+            expiresIn: process.ACCESS_TOKEN_EXPIRY
+        }
+
+
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        { // giving payload
+            _id: this._id,
+            // refresh token is used more frequently , it has only id
+
+
+
+        }, process.env.REFRESH_TOKEN_SECRET, 
+        {
+            expiresIn: process.REFRESH_TOKEN_EXPIRY
+        }
+
+
+    )
+}
+export const user = mongoose.model("User",userSchema )
